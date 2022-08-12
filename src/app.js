01b507e6312de330a99ad5e3408d082a22c9c1a0
Lisa Kurdzhieva <lizakpost@gmail.com>
2022-08-12 15:31:05 +0300

    feat: style and add icons

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const cityForm = document.querySelector('form');
 const card = document.querySelector('.card');
 const details = document.querySelector('.details');
 const time = document.querySelector('img.time');
-const icon = document.querySelector('ico.time');
+const icon = document.querySelector('.icon img');
 
 
 const updateUI = (data) => {
@@ -24,13 +24,15 @@ const updateUI = (data) => {
             </div>
   `;
 
-  let timeSrc = null;
-  if(weather.IsDayTime){
-    timeSrc = 'img/day.svg'
-  }
+  const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+  icon.setAttribute('src', iconSrc);
+
+  let timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
+  time.setAttribute('src', timeSrc);
 
   if(card.classList.contains('d-none')) card.classList.remove('d-none');
 };
+
 const updateCity = async (city) => {
 
   const cityDets = await getCity(city);
