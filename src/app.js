3e7217d55fc7879581b7ff6c46c5eac8ea836e74
Lisa Kurdzhieva <lizakpost@gmail.com>
2022-08-17 10:50:48 +0300

    feat: add some scripts and styles

--- src/app.js
+++ src/app.js
@@ -1,18 +1,13 @@
-import {
-  getCity,
-  getWeather
-} from "./forecast.js"
+import { getCity, getWeather } from "./forecast.js"
 
-const cityForm = document.querySelector('form');
-const card = document.querySelector('.card');
-const details = document.querySelector('.details');
-const time = document.querySelector('img.time');
-const icon = document.querySelector('.icon img');
-
+const cityForm = document.querySelector("form")
+const card = document.querySelector(".card")
+const details = document.querySelector(".details")
+const time = document.querySelector("img.time")
+const icon = document.querySelector(".icon img")
 
 const updateUI = (data) => {
-
-  const{ cityDets, weather} = data;
+  const { cityDets, weather } = data
 
   details.innerHTML = `
   <div class="text-muted text-uppercase text-center details">
@@ -22,35 +17,33 @@ const updateUI = (data) => {
               <span>${weather.Temperature.Metric.Value}</span>
               <span>&deg;C</span>
             </div>
-  `;
+  `
 
-  const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
-  icon.setAttribute('src', iconSrc);
+  const iconSrc = `img/icons/${weather.WeatherIcon}.svg`
+  icon.setAttribute("src", iconSrc)
 
-  let timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
-  time.setAttribute('src', timeSrc);
+  let timeSrc = weather.IsDayTime ? "img/day.svg" : "img/night.svg"
+  time.setAttribute("src", timeSrc)
 
-  if(card.classList.contains('d-none')) card.classList.remove('d-none');
-};
+  if (card.classList.contains("d-none")) card.classList.remove("d-none")
+}
 
 const updateCity = async (city) => {
+  const cityDets = await getCity(city)
+  const weather = await getWeather(cityDets.Key)
+  return { cityDets, weather }
+}
 
-  const cityDets = await getCity(city);
-  const weather = await getWeather(cityDets.Key);
-  return { cityDets, weather };
-
-};
-
-cityForm.addEventListener('submit', e => {
+cityForm.addEventListener("submit", (e) => {
   // prevent default action
-  e.preventDefault();
+  e.preventDefault()
 
   // get city value
-  const city = cityForm.city.value.trim();
-  cityForm.reset();
+  const city = cityForm.city.value.trim()
+  cityForm.reset()
 
   // update the ui with new city
   updateCity(city)
-    .then(data => updateUI(data))
-    .catch(err => console.log(err));
-});
+    .then((data) => updateUI(data))
+    .catch((err) => console.log(err))
+})
--- src/forecast.js
+++ src/forecast.js
@@ -1,27 +1,23 @@
-const key = "9DTCaL30eScb2kysS7NmwwRL6OnzrChP";
+const key = "9DTCaL30eScb2kysS7NmwwRL6OnzrChP"
 
 // get weather information
 export const getWeather = async (id) => {
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/"
+  const query = `${id}?apikey=${key}`
 
-  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
-  const query = `${id}?apikey=${key}`;
+  const response = await fetch(base + query)
+  const data = await response.json()
 
-  const response = await fetch(base + query);
-  const data = await response.json();
-
-  return data[0];
-
-};
+  return data[0]
+}
 
 // get city information
 export const getCity = async (city) => {
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search"
+  const query = `?apikey=${key}&q=${city}`
 
-  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
-  const query = `?apikey=${key}&q=${city}`;
+  const response = await fetch(base + query)
+  const data = await response.json()
 
-  const response = await fetch(base + query);
-  const data = await response.json();
-
-  return data[0];
-
-};
+  return data[0]
+}
