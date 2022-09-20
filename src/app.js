8944662f5cda485ce807f8bad49dcb6c741144b0
Lisa Kurdzhieva <lizakpost@gmail.com>
2022-09-20 12:44:22 +0300

    fix: some fixes

--- src/app.js
+++ src/app.js
@@ -1,47 +1,63 @@
+import Forecast from "./forecast.js"
+
 const cityForm = document.querySelector("form")
 const card = document.querySelector(".card")
 const details = document.querySelector(".details")
 const time = document.querySelector("img.time")
 const icon = document.querySelector(".icon img")
-const forecast = new Forecast();
 
-console.log(forecast);
+const forecast = new Forecast()
 
-// const updateUI = (data) => {
+console.log(forecast)
 
-//   const { cityDets, weather } = data
+const updateUI = (data) => {
 
-//   details.innerHTML = `
-//   <div class="text-muted text-uppercase text-center details">
-//             <h5 class="my-3">${cityDets.EnglishName}</h5>
-//             <div class="my-3">${weather.WeatherText}</div>
-//             <div class="display-4 my-4">
-//               <span>${weather.Temperature.Metric.Value}</span>
-//               <span>&deg;C</span>
-//             </div>
-//   `
+  console.log(data);
+  const { cityDets, weather } = data
 
-//   const iconSrc = `img/icons/${weather.WeatherIcon}.svg`
-//   icon.setAttribute("src", iconSrc)
+  details.innerHTML = `
+  <div class="text-muted text-uppercase text-center details">
+            <h5 class="my-3">${cityDets.EnglishName}</h5>
+            <div class="my-3">${weather.WeatherText}</div>
+            <div class="display-4 my-4">
+              <span>${weather.Temperature.Metric.Value}</span>
+              <span>&deg;C</span>
+            </div>
+  `
 
-//   let timeSrc = weather.IsDayTime ? "img/day.svg" : "img/night.svg"
-//   time.setAttribute("src", timeSrc)
+  const iconSrc = `img/icons/${weather.WeatherIcon}.svg`
+  icon.setAttribute("src", iconSrc)
 
-//   if (card.classList.contains("d-none")) card.classList.remove("d-none")
-// }
+  let timeSrc = weather.IsDayTime ? "img/day.svg" : "img/night.svg"
+  time.setAttribute("src", timeSrc)
 
-// const updateCity = async (city) => {
-//   const cityDets = await getCity(city)
-//   const weather = await getWeather(cityDets.Key)
-//   return { cityDets, weather }
-// }
+  if (card.classList.contains("d-none")) card.classList.remove("d-none")
+}
 
-// cityForm.addEventListener("submit", (e) => {
-//   e.preventDefault()
+const updateCity = async (city) => {
+  const cityDets = await forecast.getCity(city)
+  const weather = await forecast.getWeather(cityDets.Key)
+  return { cityDets, weather }
+}
 
-//   const city = cityForm.city.value.trim()
-//   cityForm.reset()
-//   updateCity(city)
-//     .then((data) => updateUI(data))
-//     .catch((err) => console.log(err))
-// })
+cityForm.addEventListener("submit", (e) => {
+  // prevent default action
+  e.preventDefault()
+
+  // get city value
+  const city = cityForm.city.value.trim()
+  cityForm.reset()
+
+  // update the ui with new city
+  forecast.updateCity(city)
+    .then((data) => updateUI(data))
+    .catch((err) => console.log(err))
+
+    localStorage.setItem('city', city);
+})
+
+if(localStorage.getItem('city')){
+  forecast.updateCity(localStorage.setItem('city'))
+  .then(data => updateUI(data))
+  .catch(err => console.log(err));
+}
--- src/forecast.js
+++ src/forecast.js
@@ -1,25 +1,31 @@
-class Forecast{
-  constructor(){
-    this.key = "9DTCaL30eScb2kysS7NmwwRL6OnzrChP";
-    this.weatherURI = "https://dataservice.accuweather.com/currentconditions/v1/";
-    this.cityURI = "https://dataservice.accuweather.com/locations/v1/cities/search";
+class Forecast {
+  constructor() {
+    this.key = "9DTCaL30eScb2kysS7NmwwRL6OnzrChP"
+    this.weatherURI =
+      "https://dataservice.accuweather.com/currentconditions/v1/"
+    this.cityURI =
+      "https://dataservice.accuweather.com/locations/v1/cities/search"
   }
-  async updateCity(city){
+
+  async updateCity(city) {
     const cityDets = await this.getCity(city)
     const weather = await this.getWeather(cityDets.Key)
     return { cityDets, weather }
   }
 
-  async getCity(city){
+  async getCity(city) {
     const query = `?apikey=${this.key}&q=${city}`
     const response = await fetch(this.cityURI + query)
     const data = await response.json()
     return data[0]
   }
-  async getWeather(id){
+
+  async getWeather(id) {
     const query = `${id}?apikey=${this.key}`
     const response = await fetch(this.weatherURI + query)
     const data = await response.json()
     return data[0]
   }
 }
+
+export default Forecast
